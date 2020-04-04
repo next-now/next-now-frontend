5261f4e5b5d328b2ca749608ceec7e838e38612e
Dan Lemon <daniel.lemon@amazee.com>
2020-04-04 23:26:06 +0200

    Submit example

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -2,10 +2,17 @@ import * as React from 'react';
 import { useTranslation } from 'react-i18next';
 import { Formik } from 'formik';
 import Button from '../components/Button';
+import Submit from '../components/Submit';
 import TextField from '../components/Formik/TextField/TextField';
 
 const LoginPage: React.FC = () => {
   const { t } = useTranslation();
+
+  const onSubmit = () => {
+    // TODO: const data = await fetch('test');
+    console.log('submit happened');
+  };
+
   return (
     <>
       <h1 className="mb-8">{t('Login')}</h1>
@@ -14,12 +21,12 @@ const LoginPage: React.FC = () => {
           'Enter your username and password to gain access to the dashboard and your token data.'
         )}
       </p>
-      <Formik initialValues={{}} onSubmit={() => {}}>
+      <Formik initialValues={{}} onSubmit={onSubmit}>
         <form>
           <TextField name="username" label="Username" />
           <TextField name="password" label="Password" type="password" />
           <Button text="Forgot password?" />
-          <Button text="Login" />
+          <Submit text="Login" />
         </form>
       </Formik>
     </>
