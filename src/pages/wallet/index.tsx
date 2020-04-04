aa75bd153b668ea688a8d3941024b64daa4be691
Dan Lemon <daniel.lemon@amazee.com>
2020-04-05 00:24:44 +0200

    Simple wallet page

--- src/pages/wallet/index.tsx
+++ src/pages/wallet/index.tsx
@@ -6,10 +6,9 @@ const Index: React.FC = () => {
   const { t } = useTranslation();
   return (
     <>
-      <h1 className="mb-8">{t('Welcome')}</h1>
+      <h1 className="mb-8">{t('Wallet')}</h1>
       <p>{t('Collaborate and level-up.')}</p>
-      <Button text="Login" path="/login" />
-      <Button text="Sign up" path="/sign-up" />
+      <Button text="Transactions" path="/wallet/transactions" />
     </>
   );
 };
