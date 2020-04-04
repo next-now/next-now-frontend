ed1f77ab8d44194463a2f81400e2f409c3ac416e
Dan Lemon <daniel.lemon@amazee.com>
2020-04-04 22:13:01 +0200

    Add initial sign-up page

--- src/pages/sign-up.tsx
+++ src/pages/sign-up.tsx
@@ -8,18 +8,15 @@ const LoginPage: React.FC = () => {
   const { t } = useTranslation();
   return (
     <>
-      <h1 className="mb-8">{t('Login')}</h1>
-      <p className="mb-4">
-        {t(
-          'Enter your username and password to gain access to the dashboard and your token data.'
-        )}
-      </p>
+      <h1 className="mb-8">{t('Register')}</h1>
+      <p className="mb-4">{t('Enter your details to create an account.')}</p>
       <Formik initialValues={{}} onSubmit={() => {}}>
         <form>
-          <TextField name="username" label="Username" />
-          <TextField name="password" label="Password" type="password" />
-          <Button text="Forgot password?" />
-          <Button text="Login" />
+          <TextField name="name" label="Username" />
+          <TextField name="email" label="Email" />
+          <TextField name="username" label="Password" type="password" />
+          <TextField name="password" label="Confirm Password" type="password" />
+          <Button text="Submit" />
         </form>
       </Formik>
     </>
