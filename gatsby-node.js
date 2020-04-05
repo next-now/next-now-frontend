ce30de43d08c595b9cdc10bd1687c802a8abfb5a
Dan Lemon <daniel.lemon@amazee.com>
2020-04-05 15:38:15 +0200

    Remove layout

--- gatsby-node.js
+++ gatsby-node.js
@@ -9,12 +9,23 @@ exports.onCreatePage = async ({ page, actions }) => {
     Object.keys(languages).forEach(lang => {
       const languagePrefix = lang === defaultLanguage ? '' : `${lang}/`;
 
+      // Rewriting the `match` property has to be done for each page that contains
+      // client-side routing.
+      // Possible alternatives:
+      //    - https://www.gatsbyjs.org/packages/gatsby-plugin-create-client-paths/
+      //    - use NGINX config to always serve up base page
+      if (page.path.match(/^\/wallet/)) {
+        // page.matchPath is a special key that's used for matching pages
+        // with corresponding routes only on the client.
+        page.matchPath = `/${languagePrefix}wallet/*`;
+      }
+
       return createPage({
         ...page,
         path: `/${languagePrefix}${page.path.substring(1)}`,
         context: {
           language: lang,
-          layout: page.path.match(/auth/) ? 'auth' : 'anonymous',
+          languagePrefix,
         },
       });
     });
