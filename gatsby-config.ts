ae8d3a821e82ffc79c0c19cef3f257dde0b17c9a
bejitono <stdemicheli@protonmail.com>
2020-04-05 16:49:05 +0700

    Removes initiatives page from main nav.

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -27,7 +27,6 @@ export const siteMetadata = {
     { path: '/', label: 'Home' },
     { path: '/wallet', label: 'Wallet' },
     { path: '/dashboard', label: 'Dashboard' },
-    { path: '/createInitiative', label: 'Create Initiative' },
   ],
 };
 
